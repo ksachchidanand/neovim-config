540c537b87fb8fde053d1a931f91618994a765ca
ksachchidanand <sdkr.nitkkr@gmail.com>
2023-05-20 13:02:48 +0530

    disable a bunch of not useful to me plugin

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,11 @@
+return {
+  { "L3MON4D3/LuaSnip",             enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "hrsh7th/nvim-cmp",             enabled = false },
+  { "hrsh7th/cmp-nvim-lsp",         enabled = false },
+  { "hrsh7th/cmp-buffer",           enabled = false },
+  { "hrsh7th/cmp-path",             enabled = false },
+  { "saadparwaiz1/cmp_luasnip",     enabled = false },
+  { "akinsho/bufferline.nvim",      enabled = false },
+  { "folke/tokyonight.nvim",        enabled = false }
+}
