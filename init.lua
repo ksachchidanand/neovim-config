041760d62227197b195625d9df17356fbb871395
ksachchidanand <sdkr.nitkkr@gmail.com>
2023-07-21 04:50:20 -0400

    restarting config with packer

--- init.lua
+++
@@ -1,4 +0,0 @@
--- [[ ~/.config/nvim/init.lua ]]
-
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
--- lua/plugins/disabled.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { "L3MON4D3/LuaSnip",             enabled = false },
-  { "rafamadriz/friendly-snippets", enabled = false },
-  { "saadparwaiz1/cmp_luasnip",     enabled = false },
-  { "akinsho/bufferline.nvim",      enabled = false },
-  { "folke/tokyonight.nvim",        enabled = false }
-}
